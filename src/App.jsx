41861bc1153831076282b6d06f7891d30e1813a8
Misty <mistybarnes79@gmail.com>
2023-09-06 14:40:34 -0400

    pups

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { puppyList } from "./data.js";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  console.log(featuredPup);
+  return (
+    <div>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
